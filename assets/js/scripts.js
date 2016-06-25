dca723bc1b2ce160ca8f632bc49239933bcb3d90
elliottcandrew <luciapells@Slumlordbillionaire.local>
2016-06-25 18:02:07 +0200

    trying to workout flex within jspPane

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -25,6 +25,31 @@ $(window).load(function() {
 		}
 	);
 
+	$('.container2').each(
+		function()
+		{
+			$(this).jScrollPane();
+			var api = $(this).data('jsp');
+			var throttleTimeout;
+			$(window).bind(
+				'resize',
+				function()
+				{
+					if (!throttleTimeout) {
+						throttleTimeout = setTimeout(
+							function()
+							{
+								api.reinitialise();
+								throttleTimeout = null;
+							},
+							50
+						);
+					}
+				}
+			);
+		}
+	);
+
 if ($('.container').css('display') == 'block') {
 
 	$('.container').each(
