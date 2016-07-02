5b4cc1c39622a8431d67cfec9a2878b95ae362c8
elliottcandrew <luciapells@iPhonevnWouter3.local>
2016-07-02 13:15:25 +0200

    trying to fix scroll not disappearing

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -51,36 +51,10 @@ $(window).load(function() {
 				);
 			}
 		);
-
 	}
 
-// if ($('.container').css('display') == 'block') {
-//
-// 	$('.container').each(
-// 		function()
-// 		{
-// 			$(this).jScrollPane();
-// 			var api = $(this).data('jsp');
-// 			var throttleTimeout;
-// 			$(window).bind(
-// 				'resize',
-// 				function()
-// 				{
-// 					if (!throttleTimeout) {
-// 						throttleTimeout = setTimeout(
-// 							function()
-// 							{
-// 								api.reinitialise();
-// 								throttleTimeout = null;
-// 							},
-// 							50
-// 						);
-// 					}
-// 				}
-// 			);
-// 		}
-// 	);
-//
-// 	}
+	else {
+
+	}
 
 });
