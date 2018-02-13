47362721f1eb0377ae6ad21e203eb2dec3d7eae1
Elliott Andrew <elliottcandrew@gmail.com>
2018-02-13 19:09:44 +0100

    Blog layout and other minor changes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -34,14 +34,14 @@ $(document).ready(function() {
     return false;
   });
 
-  $('.tab-right, .close-2').on('click', function() {
-    $('.menu2, .tab-right').toggleClass('clicked');
-    return false;
-  });
+  // $('.tab-right, .close-2').on('click', function() {
+  //   $('.menu2, .tab-right').toggleClass('clicked');
+  //   return false;
+  // });
 
   $('html').on('click', function() {
     $('.menu1, .tab-left').removeClass('clicked');
-    $('.menu2, .tab-right').removeClass('clicked');
+    // $('.menu2, .tab-right').removeClass('clicked');
   });
 
   // TOUCH MENU
